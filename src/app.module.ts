9d334719ce0be8eb8839fb60ad490ce5f8b1169a
unknown <ahmedminhaz17@gmail.com>
2022-08-25 18:00:19 +0600

    user deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost:27017/testDB'), UsersModule],
-  controllers: [AppController, UsersController],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost:27017/testDB')],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
